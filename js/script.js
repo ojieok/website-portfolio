86674423e8bf55be72e1d638041779cc0a37cb3f
ojieok <142185852+ojieok@users.noreply.github.com>
2024-04-01 00:17:01 -0400

    change

--- js/script.js
+++ js/script.js
@@ -1,8 +1,12 @@
+document.addEventListener("DOMContentLoaded", () =>{
+
 let time = document.getElementById("timep");
 
 setInterval(() =>{
     let d = new Date();
     time.innerHTML = d.toLocaleTimeString();
 }, 1000)
+
+});
 
 
